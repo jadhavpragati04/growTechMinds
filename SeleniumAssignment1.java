cb957526afd2d9d3729f65bac8380683ab66920a
Pragati <jadhavpragati04@gmail.com>
2024-04-27 14:53:46 +0530

    Assignment to launch empty browser

---
+++ SeleniumAssignment1.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumAssignment1 {
+	public static void main(String[] args) {
+	
+	ChromeDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com/");
+
+}
+}

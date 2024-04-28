01d1d1e034bcfbb7aa9841fbbb51f0a7837ecbf2
Pragati <jadhavpragati04@gmail.com>
2024-04-28 14:07:50 +0530

    Program to launch the google and click on Images link

---
+++ SeleniumAssignment13.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumAssignment13 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		WebElement imgLink = driver.findElement(By.linkText("Images"));
+		Thread.sleep(1000);
+		imgLink.click();
+		System.out.println("Clicked on the link successfully");
+		driver.close();
+
+	}
+
+}
